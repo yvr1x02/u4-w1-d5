c8c00fed11721ae4bbcfe97ee514abc8df81b2ca
Yuri Impellizzeri <128586274+yvr1x02@users.noreply.github.com>
2024-07-26 10:12:49 +0200

    created ElementoMultimediale

---
+++ src/entities/ElementoMultimediale.java
@@ -0,0 +1,18 @@
+package entities;
+
+public abstract class ElementoMultimediale {
+
+    protected String titolo;
+
+
+    public ElementoMultimediale(String titolo) {
+        this.titolo = titolo;
+
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    
+}
